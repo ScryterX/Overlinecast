fcc85fe958e44fc8a802a45ae2de565eea2bd2ed
ScryterX <josielcardosomercio@gmail.com>
2023-10-18 17:43:25 -0300

    develop

--- src/modules/routes/dashboard.js
+++ src/modules/routes/dashboard.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+// Em seu controlador
+const serverInfo = {
+  ip: "localhost",
+  port: 1935, // Ou a porta que você está usando
+  app: "live", // Nome do aplicativo RTMP
+};
+
 router.get("/", (req, res) => {
-  res.render("users/dashboard");
+  res.render("users/dashboard", { serverInfo });
 });
 
 module.exports = router;
